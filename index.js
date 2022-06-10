69603e65b7cd2c2cad653f2f8104112a3ff7c970
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-10 11:24:28 -0400

    commit tree 72a769715bd3869665f28693c6dc58d5bf403649

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(saturdayActivity = "roller-skate"){
+    return `This Saturday, I want to ${saturdayActivity}!`
+};
+saturdayFun();
+
+const mondayWork = function(mondayActivity = "go to the office"){
+    return `This Monday, I will ${mondayActivity}.`
+};
+mondayWork();
+
+function wrapAdjective(visualFlair = "*"){
+    return function(adjective = "special"){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    };
+}
+wrapAdjective("%")("a dedicated programmer");
